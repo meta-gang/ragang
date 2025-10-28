37738519c4e8919e6ff7a88f03d438a57edf1450
Park-SeungWoo <tmd24710@naver.com>
2025-10-29 00:56:47 +0900

    fix: recover flow_id in container obj

--- exceptions/user/modules.py
+++
@@ -1,4 +0,0 @@
-class UnlinkedModuleException(Exception):
-    def __init__(self, src_mid: str, unlinked: list[str]):
-        super().__init__(f"Module '{src_mid}' attempted to call unlinked modules '{unlinked}'."
-                         f"Check '{src_mid}.output['next']'")
